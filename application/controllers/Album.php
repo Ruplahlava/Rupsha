14776bff3ba6e0c3863282f868ebc0c66d7d8b4f
Štěpán Postránecký <stepan.postranecky@gmail.com>
2015-01-04 20:13:45 +0100

    album frame

--- application/controllers/Album.php
+++ application/controllers/Album.php
@@ -13,12 +13,55 @@
  */
 class Album extends CI_Controller {
 
+    const ALBUM_VIEW = '';
+    const WELCOME_VIEW = '';
+    const ERROR_VIEW = '';
+    
+    
     public function __construct() {
         parent::__construct();
     }
+
+    /**
+     * @description Main viewing method
+     * @param string $hash
+     */
+    public function index($hash = NULL) {
+        $album = $this->foto->get_album(array('hash' => $hash));
+        if (NULL === $hash) {
+            // welcome page
+            $this->_welcome();
+        } elseif (FALSE !== $album) {
+            // album content
+            $this->_show_album($album);
+        } else {
+            // 404
+            $this->_show_error();
+        }
+    }
     
-    public function index($hash=NULL) {
+    /**
+     * 
+     */
+    public function _welcome() {
         
     }
+    
+    /**
+     * 
+     * @param array $album
+     */
+    public function _show_album($album) {
+        
+    }
+    
+    /**
+     * 
+     */
+    public function _show_error() {
+        
+    }
+    
+    
 
 }
