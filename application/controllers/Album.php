1e62eb1283679db789c2c2b471a1cd62e4e84dc2
Štěpán Postránecký <stepan.postranecky@gmail.com>
2015-01-04 16:50:42 +0100

    Default controller Album intend to view users album

---
+++ application/controllers/Album.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Album
+ *
+ * @author Parek
+ */
+class Album extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function index($hash=NULL) {
+        
+    }
+
+}
