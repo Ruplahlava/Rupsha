76fcd368ece8f813cad0efa0407260ca7276cdd0
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-06 11:38:17 +0100

    google analytics separation

--- application/views/album/footer.php
+++ application/views/album/footer.php
@@ -1,7 +1,9 @@
       <hr>
-
       <footer>
-        <p>&copy; Company 2014</p>
+          <iframe src="http://ghbtns.com/github-btn.html?user=Ruplahlava&repo=Rupsha&type=watch"
+                  allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
+          <iframe src="http://ghbtns.com/github-btn.html?user=Ruplahlava&repo=Rupsha&type=fork"
+                  allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
       </footer>
     </div> <!-- /container -->        
 
@@ -9,14 +11,6 @@
 
         <script src="<?= base_url() ?>js/main.js"></script>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
+        <?php $this->load->view('google_analytics'); ?>
     </body>
 </html>
--- application/views/footer.php
+++ application/views/footer.php
@@ -10,14 +10,6 @@
 
         <script src="<?= base_url() ?>js/main.js"></script>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
+        <?php $this->load->view('google_analytics'); ?>
     </body>
 </html>
