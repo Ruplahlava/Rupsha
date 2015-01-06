9afe656218bc4064c2c1ce2fb4d2f11b14262d23
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-06 07:58:09 +0100

    show Name and location of album

--- application/views/album/header.php
+++ application/views/album/header.php
@@ -3,3 +3,5 @@
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     <div class="container">
+        <h1><?= $album[0]->name ?></h1>
+        <h3><?= $album[0]->place ?></h3>
