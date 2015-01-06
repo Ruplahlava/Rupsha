7afd4951eaf72a7bd76d6ff49bc0272c258f7da8
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-06 11:59:42 +0100

    footer change to git address

--- application/views/album/footer.php
+++ application/views/album/footer.php
@@ -1,9 +1,9 @@
       <hr>
-      <footer>
-          <iframe src="http://ghbtns.com/github-btn.html?user=Ruplahlava&repo=Rupsha&type=watch"
+      <footer> <p class="pull-right">Powered by <a href="https://github.com/Ruplahlava/Rupsha">Rupsha</a></p> <iframe src="http://ghbtns.com/github-btn.html?user=Ruplahlava&repo=Rupsha&type=watch"
                   allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
           <iframe src="http://ghbtns.com/github-btn.html?user=Ruplahlava&repo=Rupsha&type=fork"
                   allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
+          
       </footer>
     </div> <!-- /container -->        
 
