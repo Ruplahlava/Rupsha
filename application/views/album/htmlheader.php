3c4251c12269b9519799d878598bc6ac18c58fb1
Štěpán Postránecký <stepan.postranecky@gmail.com>
2015-01-06 18:10:23 +0100

    Open Graph tag for image preview

--- application/views/album/htmlheader.php
+++ application/views/album/htmlheader.php
@@ -2,12 +2,13 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title><?=$title?></title>
         <meta name="description" content="">
+            <meta property="og:image" content="<?= base_url() ?>img/user/<?= $user[0]->login ?>/<?= $album[0]->id ?>/<?= $photo[0]->name ?>_thumb<?= $photo[0]->extension ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="<?= base_url() ?>css/bootstrap.min.css">
--- application/views/album/nanoSettings.php
+++ application/views/album/nanoSettings.php
@@ -5,10 +5,10 @@
             items: [
 <?php foreach ($photo as $value): ?>
                     {
-                        src: '<?= base_url() ?>img/user/<?= $user[0]->login ?>/<?= $album[0]->id ?>/<?= $value->name ?>_wm<?= $value->extension ?>', // image url
-                        srct: '<?= base_url() ?>img/user/<?= $user[0]->login ?>/<?= $album[0]->id ?>/<?= $value->name ?>_thumb<?= $value->extension ?>', // thumbnail url
-                        title: '<?= $album[0]->name ?>', // thumbnail title
-                        description: '<?= $value->text ?>'		// thumbnail description
+                        src: '<?= base_url() ?>img/user/<?= $user[0]->login ?>/<?= $album[0]->id ?>/<?= $value->name ?>_wm<?= $value->extension ?>', 
+                        srct: '<?= base_url() ?>img/user/<?= $user[0]->login ?>/<?= $album[0]->id ?>/<?= $value->name ?>_thumb<?= $value->extension ?>',
+                        title: '<?= $album[0]->name ?>',
+                        description: '<?= $value->text ?>'
                     }<?php echo end($photo) == $value ? '': ','; ?>
 <?php endforeach; ?>
             ],
