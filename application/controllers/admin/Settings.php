030574c92fb9e7bebd185893fb6e1e65f3d46ba4
Štěpán Postránecký <stepan.postranecky@gmail.com>
2015-01-07 20:29:12 +0100

    New settings controller

---
+++ application/controllers/admin/Settings.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Description of Settings
+ *
+ * @author Parek
+ */
+class Settings extends CI_Controller
+{
+    protected $data;
+
+    const USERS_VIEW = '';
+    const ACCOUNT_VIEW = '';
+    const GALLERY_VIEW = '';
+    const PAGE_VIEW = '';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function users()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function account()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function gallery()
+    {
+        
+    }
+
+    /**
+     * 
+     */
+    public function page()
+    {
+        
+    }
+
+}
