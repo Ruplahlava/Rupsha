f84692230df6e2084ea8f04186cf2548a4569832
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-26 08:16:10 +0100

    nanogallery setting fn for hits - getting filename

--- application/views/album/nanoSettings.php
+++ application/views/album/nanoSettings.php
@@ -1,25 +1,36 @@
 <script>
     $(document).ready(function () {
-    $("#nanoAlbum").nanoGallery({
-    thumbnailWidth: 'auto', thumbnailHeight: 180,
+        function myImgToolbarCustInit(elementName) {
+                return '';
+        }
+        function setHit($elements, item, data) {
+            var filename = item.src.substring(item.src.lastIndexOf('/')+1);
+        }
+        $("#nanoAlbum").nanoGallery({
+            thumbnailWidth: 'auto', 
+            thumbnailHeight: 180,
             items: [
-                <?php foreach ($photo as $value): ?>
-                    {
+                <?php foreach ($photo as $value): ?>{
                         src: '<?= base_url() ?>img/user/<?= $user[0]->login ?>/<?= $album[0]->id ?>/<?= $value->name ?>_wm<?= $value->extension ?>',
                         srct: '<?= base_url() ?>img/user/<?= $user[0]->login ?>/<?= $album[0]->id ?>/<?= $value->name ?>_thumb<?= $value->extension ?>',
                         title: '<?= $album[0]->name ?>',
                         description: '<?= $value->text ?>'
-                    }
-                <?php echo end($photo) == $value ? '' : ','; ?>
+                    }<?php echo end($photo) == $value ? '' : ','; ?>
                 <?php endforeach; ?>
                     ],
-                                            thumbnailHoverEffect: 'scale120,borderLighter',
-                                            thumbnailLabel: {
-                                            display: false
-                                            }                                   ,
-                                            colorScheme: 'light',
-                                            colorSchemeViewer: 'light',
-                                            theme: 'light'
+            thumbnailHoverEffect: 'scale120,borderLighter',
+            viewerToolbar: {
+                autoMinimize: 0,
+                standard: 'closeButton,previousButton,nextButton,label,custom1'
+            },
+            thumbnailLabel: {
+            display: false
+            },
+            colorScheme: 'light',
+            colorSchemeViewer: 'light',
+            theme: 'light',
+            fnImgToolbarCustInit: myImgToolbarCustInit,
+            fnImgToolbarCustDisplay: setHit
         });
     });
 </script>
