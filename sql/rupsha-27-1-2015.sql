63fca340a498338d412e8322af7f18845f814e1d
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-27 16:42:36 +0100

    new db version

---
+++ sql/rupsha-27-1-2015.sql
@@ -0,0 +1,78 @@
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE IF NOT EXISTS `album` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(400) COLLATE utf8_czech_ci NOT NULL,
+  `text` text COLLATE utf8_czech_ci NOT NULL,
+  `place` varchar(400) COLLATE utf8_czech_ci NOT NULL,
+  `hits` int(11) NOT NULL,
+  `hash` varchar(10) COLLATE utf8_czech_ci NOT NULL,
+  `locked` tinyint(1) NOT NULL,
+  `password` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) COLLATE utf8_czech_ci NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) COLLATE utf8_czech_ci NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) COLLATE utf8_czech_ci NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foto`
+--
+
+CREATE TABLE IF NOT EXISTS `foto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_album` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `text` text COLLATE utf8_czech_ci NOT NULL,
+  `hits` int(11) NOT NULL,
+  `extension` varchar(5) COLLATE utf8_czech_ci NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_settings`
+--
+
+CREATE TABLE IF NOT EXISTS `page_settings` (
+  `ga` varchar(40) COLLATE utf8_czech_ci NOT NULL DEFAULT 'UA-XXXXX-X',
+  `max_dimension` int(11) NOT NULL DEFAULT '0',
+  `quality` int(11) NOT NULL DEFAULT '90'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(200) COLLATE utf8_czech_ci NOT NULL,
+  `password` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `admin` int(1) NOT NULL DEFAULT '0',
+  `ip` varchar(60) COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
