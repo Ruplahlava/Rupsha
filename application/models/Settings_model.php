b651495fdee260b4b8067286f04f8762fce4ca03
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-29 07:56:18 +0100

    setting values in db

--- application/models/Settings_model.php
+++ application/models/Settings_model.php
@@ -1,13 +1,29 @@
 <?php
-class Settings_model extends CI_Model {
 
-     public function get_settings()
-     {
-         $settings = $this->db->get('page_settings');
-         if($settings->num_rows() === 0){
-             $this->db->query('INSERT INTO page_settings VALUES() ');
-             $settings = $this->db->get('page_settings');
-         }
-         return $settings->result();
-     }
+class Settings_model extends CI_Model
+{
+
+    /**
+     * 
+     * @return object
+     */
+    public function get_page_settings()
+    {
+        $settings = $this->db->get('page_settings');
+        if ($settings->num_rows() === 0) {
+            $this->db->query('INSERT INTO page_settings VALUES() ');
+            $settings = $this->db->get('page_settings');
+        }
+        return $settings->result();
+    }
+
+    /**
+     * 
+     * @param array $post
+     */
+    public function set_page_settings($post)
+    {
+       return $this->db->update('page_settings', $post, 'id=1');
+    }
+
 }
