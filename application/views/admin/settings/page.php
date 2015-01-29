1ba98b361ac9ae934b08b79c44a11b26faf966f1
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-29 08:25:51 +0100

    Info box for settings

--- application/views/admin/settings/page.php
+++ application/views/admin/settings/page.php
@@ -2,8 +2,8 @@
 <?php $this->load->view('admin/header'); ?>
 <div class="row">
     <?php $this->load->view('admin/settings/alert'); ?>
-    <div class="col-md-6">
-        <h3>Page settings</h3>
+    <h3>Page settings</h3>
+    <div class="col-md-6">        
         <form class="form-horizontal" method="post" action="<?= base_url()?>admin/settings/page_set">
             <div class="form-group">
                 <label for="ganal" class="col-sm-3 control-label">Google analytics</label>
@@ -11,6 +11,7 @@
                     <input type="text" class="form-control" id="ganal" placeholder="Google analytics code" name="ga" value="<?= $settings[0]->ga?>">
                 </div>
             </div>
+            <h5>Picture settings</h5>
             <div class="form-group">
                 <label for="inputEmail3" class="col-sm-3 control-label">Max dimension</label>
                 <div class="col-sm-9">
@@ -29,6 +30,13 @@
                 </div>
             </div>
         </form>
+    </div>
+    <div class="col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                Picture settings is applied for new pictures only!
+            </div>
+        </div>
     </div>
 </div>
 <?php $this->load->view('admin/footer'); ?>
