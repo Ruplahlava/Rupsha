483a88871b70e84009b998e449e0b932779dc851
POSTRS01 <POSTRS01@BRNOLPT-DEV29.DSG.DSGROOT.INT>
2015-01-30 08:13:00 +0100

    404 fix jumbotron

--- application/views/404.php
+++ application/views/404.php
@@ -1,8 +1,8 @@
 <?php $this->load->view('htmlheader'); ?>
 <div class="container">
   <div class="row">
-    <div class="span12">
-      <div class="hero-unit center">
+    <div class="col-md-12">
+      <div class="jumbotron center">
           <h1>Album Not Found <small><font face="Tahoma" color="red">Error 404</font></small></h1>
           <br />
           <p>The page you requested could not be found. Please check if your URL is correct or if album you are looking for still exists. You can use your browsers <b>Back</b> button to navigate to the page you have previously come from.</p>
