389170dcd2fc65c94f7042f6af8417b343318815
Stepan Postranecky <stepan.postranecky@gmail.com>
2015-04-09 15:19:22 +0200

    Update to Codeigniter 3 - session update

---
+++ sql/session_update.sql
@@ -0,0 +1,9 @@
+DROP TABLE `ci_sessions`;
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+        `id` varchar(40) NOT NULL,
+        `ip_address` varchar(45) NOT NULL,
+        `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        `data` blob NOT NULL,
+        PRIMARY KEY (id),
+        KEY `ci_sessions_timestamp` (`timestamp`)
+);
