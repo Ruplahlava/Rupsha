013515ac6894b2a5ce9b8d19abdd83517dfbeb17
Stepan Postranecky <stepan.postranecky@gmail.com>
2015-04-15 08:01:38 +0200

    Mainpage + codeigniter 3 fix

--- sql/install.sql.sql
+++ sql/install.sql.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS `album` (
   `hash` varchar(10) COLLATE utf8_czech_ci NOT NULL,
   `locked` tinyint(1) NOT NULL,
   `password` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+  `hidden` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
@@ -23,13 +24,12 @@ CREATE TABLE IF NOT EXISTS `album` (
 --
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
-  `session_id` varchar(40) COLLATE utf8_czech_ci NOT NULL DEFAULT '0',
-  `ip_address` varchar(45) COLLATE utf8_czech_ci NOT NULL DEFAULT '0',
-  `user_agent` varchar(120) COLLATE utf8_czech_ci NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text COLLATE utf8_czech_ci NOT NULL,
-  PRIMARY KEY (`session_id`),
-  KEY `last_activity_idx` (`last_activity`)
+  `id` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `ip_address` varchar(45) COLLATE utf8_czech_ci NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ci_sessions_timestamp` (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 -- --------------------------------------------------------
@@ -61,6 +61,9 @@ CREATE TABLE IF NOT EXISTS `page_settings` (
   `ga` varchar(40) COLLATE utf8_czech_ci NOT NULL DEFAULT 'UA-XXXXX-X',
   `max_dimension` int(11) NOT NULL DEFAULT '0',
   `quality` int(11) NOT NULL DEFAULT '90',
+  `mainpage` int(11) NOT NULL DEFAULT '0',
+  `mainpage_text` text COLLATE utf8_czech_ci NOT NULL,
+  `mainpage_style` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
