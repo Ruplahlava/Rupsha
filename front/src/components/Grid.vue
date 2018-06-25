a67408bba081156f7e334ecb29780498bd4bbf6a
Stepan Postranecky <stepan.postranecky@gmail.com>
2018-06-25 21:34:13 +0200

    Data cleanup

---
+++ front/src/components/Grid.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: 'Grid',
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
